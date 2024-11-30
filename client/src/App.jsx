c21e805c64e6e4319dc8d162bf7f4422a77c7046
mahnoosh <mahnooshekrami@gmai.com>
2024-12-01 01:30:47 +0330

    add initial styles

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-primary-700 bg-opacity-10 container  '>
+      شروع پروژه
+    </div>
+  )
+}
+
+export default App
